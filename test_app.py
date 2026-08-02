aa3c3e4a07c6ab5d015324c12fb95789fa35a0c4
vikhayatrana0110-crypto <vikhayatrana0110@gmail.com>
2026-08-02 22:37:31 +0530

    test_fix

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     response=app.test_client().get("/")
 
     assert response.status_code == 200
-    assert response.data== b"hello world"
+    assert response.data== b"Hello World"
